c29d1660c89903711fb93203ea98704b4c0c82e8
两好三坏 <771909584@qq.com>
2022-06-22 09:19:34 +0800

    push reg code

--- testRegex/reg/main.go
+++ testRegex/reg/main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"fmt"
+	"regexp"
+)
+
+//
+//type regStruct struct {
+//	rexStr string
+//	rexCode string
+//}
+
+/// 代码中的正则表达式均来自 : https://regexlearn.com/zh-cn/learn/regex101
+
 func main() {
-	$END$
+	testEndPix("demo.pdf")
+}
+
+func testEndPix(fileName string) {
+	s0, r0 := "/OK/g", "Understand? OK or Not"
+	s1, r1 := "demo.pdf", "^\\w+\\.pdf$"
+	s2, r2 := "abcedf", "/[e-o]/g"
+
+	tempRegList := []string{r0, r1, r2}
+	tempStrList := []string{s0, s1, s2}
+
+	for i := 0; i < len(tempRegList); i++ {
+		fileEnd, err := regexp.MatchString(tempRegList[i], tempStrList[i])
+		fmt.Println(fileEnd, err)
+	}
+
 }
